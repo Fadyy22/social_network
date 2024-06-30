04e644b1cd75007e8a146cb0d149dce63085fc38
Fadyy22 <fadyalaa441@gmail.com>
2024-06-30 22:32:25 +0200

    make signup and login endpoints

---
+++ controllers/authController.js
@@ -0,0 +1,20 @@
+const asyncHandler = require('express-async-handler');
+const { PrismaClient } = require('@prisma/client');
+
+const createToken = require('../utils/createToken');
+const prisma = new PrismaClient();
+
+exports.signup = asyncHandler(async (req, res) => {
+  const user = await prisma.user.create({
+    data: req.body,
+  });
+
+  const token = createToken(user.id);
+
+  res.status(201).json({ user, token });
+});
+
+exports.login = asyncHandler(async (req, res) => {
+  const token = createToken(req.user.id);
+  res.status(200).json({ user: req.user, token });
+});
---
+++ utils/validators/authValidator.js
@@ -0,0 +1,98 @@
+const { check, checkExact } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const { PrismaClient } = require('@prisma/client');
+
+const {
+  customValidatorMiddleware,
+  globalValidatorMiddleware,
+} = require('../../middlewares/validatorMiddleware');
+
+const prisma = new PrismaClient();
+
+exports.signupValidator = [
+  check('firstName')
+    .trim()
+    .notEmpty()
+    .withMessage('First name is required')
+    .isString()
+    .withMessage('First name must be only characters')
+    .isLength({ min: 2, max: 50 })
+    .withMessage('First name must be between 2 and 50 characters'),
+  check('lastName')
+    .trim()
+    .notEmpty()
+    .withMessage('Last name is required')
+    .isString()
+    .withMessage('Last name must be only characters')
+    .isLength({ min: 2, max: 50 })
+    .withMessage('Last name must be between 2 and 50 characters'),
+  check('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Email is not valid')
+    .bail()
+    .custom(async (email, { req }) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (user) {
+        req.customError = {
+          message: 'Email is already taken',
+          statusCode: 409,
+        };
+      }
+    }),
+  customValidatorMiddleware,
+  check('password')
+    .trim()
+    .isStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })
+    .withMessage('Password must have a minimum length of 8 characters, with at least one lowercase letter, one uppercase letter, one number, and one special character')
+    .customSanitizer(password => bcrypt.hashSync(password, 12)),
+  checkExact([], { message: 'Unknown fileds' }),
+  globalValidatorMiddleware,
+];
+
+exports.loginValidator = [
+  check('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Email is not valid'),
+  check('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password is required')
+    .bail()
+    .custom(async (password, { req }) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      if (!user) {
+        req.customError = {
+          message: 'Invalid email or password',
+          statusCode: 401,
+        };
+      } else {
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+          req.customError = {
+            message: 'Invalid email or password',
+            statusCode: 401,
+          };
+        }
+      }
+      req.user = user;
+    }),
+  customValidatorMiddleware,
+  globalValidatorMiddleware,
+];
