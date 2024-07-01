f0a9063dacc5122549cf28539c95d251b0e89b34
Fadyy22 <fadyalaa441@gmail.com>
2024-07-01 16:41:10 +0200

    update number of comments on posts when creating or deleting

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -12,6 +12,17 @@ exports.createComment = asyncHandler(async (req, res) => {
     }
   });
 
+  await prisma.post.update({
+    where: {
+      id: req.params.postId,
+    },
+    data: {
+      commentsCount: {
+        increment: 1,
+      }
+    }
+  });
+
   res.status(201).json({ comment });
 });
 
@@ -19,6 +30,17 @@ exports.deleteComment = asyncHandler(async (req, res) => {
   await prisma.comment.delete({
     where: {
       id: req.params.id,
+    }
+  });
+
+  await prisma.post.update({
+    where: {
+      id: req.params.postId,
+    },
+    data: {
+      commentsCount: {
+        decrement: 1,
+      }
     }
   });
 
